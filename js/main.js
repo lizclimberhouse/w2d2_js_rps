347c789cb31773865f00b71244d2c82dafea6d01
Liz House <lizehouse@gmail.com>
2018-02-13 18:16:08 -0700

    game working

--- js/main.js
+++ js/main.js
@@ -3,41 +3,45 @@ console.log("this JS is working")
 
 var user = document.getElementById('user')
 var comp = document.getElementById('comp')
-
+var result = document.getElementById('result')
 var compArr = ["rock", "paper", "scissors"]
+var win_loose 
+
+
 
 function startGame(e) {
-  console.log(e.target.id)
+  // console.log(e.target.id)
   var userChoice = e.target.id
-  // var compChoice = compArr[Math.floor(Math.random() * compArray.length)]
-  var compChoice = Math.random(1);
-  if (compChoice < 0.5) {
-    compChoice = "rock"
-  }
-  else if (compChoice > 0.34 && compChoice < 0.66) {
-    compChoice = "paper"
-  }
-  else {
-    compChoice = "scissors"
-  }
-  console.log(compChoice)
-  // var compChoice = compArray[compIndex]
-  // var compChoice = rand(string of 3)
-  // var compChoice
+  // var compChoice = compArr[Math.floor(Math.random() * compArray.length)] // Couldn't get this to work
+  var compChoice = Math.random();
+    if (compChoice < 0.5) {
+      compChoice = "rock"
+    }
+    else if (compChoice > 0.34 && compChoice < 0.66) {
+      compChoice = "paper"
+    }
+    else {
+      compChoice = "scissors"
+    }
+  // console.log(compChoice)
   user.innerHTML = userChoice
   comp.innerHTML = compChoice
-  asses(userChoice, compChoice)
+  assess(userChoice, compChoice)
 }
 
-// function assess(a, b) {
-//   if  ===
-//     print out win or loose
-//     psuh onto array tally how may times won
-//   else if  ||
-//   else if ||
-//   else
-
-// }
+function assess(userChoice, compChoice) {
+  if  (userChoice === compChoice) {
+    win_loose = "You Tied"
+  }
+  else if (userChoice === "rock" && compChoice === "paper" || userChoice === "paper" && compChoice === "scissors" || userChoice === "scissors" && compChoice === "rock" ) {
+    win_loose = "You Loose"
+  }
+  else {
+    win_loose = "You Win!"
+  }
+  // console.log(win_loose)
+  result.innerHTML = win_loose
+}
 
 var choices = document.getElementsByClassName('choice')
 for (var i in choices) {
